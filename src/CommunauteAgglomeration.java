4c1a3fee38a16a34a4c562e2400dc347bbfb25b3
Ryns37 <125963159+Ryns37@users.noreply.github.com>
2023-12-18 00:48:58 +0100

    Update CommunauteAgglomeration.java

--- src/CommunauteAgglomeration.java
+++ src/CommunauteAgglomeration.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 
 public class CommunauteAgglomeration {
