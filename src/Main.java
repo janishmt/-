f9f4827810c3a01dfa71f3854666cad09e8cd73a
janishmt <152104861+janishmt@users.noreply.github.com>
2023-12-17 14:01:35 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,97 +1,35 @@
+package projetS5;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-/*Methode principale du projet*/
+
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int nombreVilles;
-        
-        do{
-        System.out.println("Entrez le nombre de villes (max 26):");
-        nombreVilles = scanner.nextInt();
-         scanner.nextLine();
-        }while(nombreVilles<=0||nombreVilles>26);
-        CommunauteAgglomeration ca = new CommunauteAgglomeration(nombreVilles);
-        
-        for(int i=0;i<nombreVilles;i++){
-            System.out.println("veuillez entrer la ville numéro"+ " "+ (i+1));
-            char ville=scanner.nextLine().toUpperCase().charAt(0);  /*récupérer la ville tapée par l'utilisateur */
-            ca.ajouterVille(ville);
-            
-        }
-      
 
-       
-           
-          
-        
-        
+        CommunauteAgglomeration ca;
 
-        /*  Menu pour ajouter des routes*/
-        boolean fin = false;
-        
-        
-        
-        while (!fin) {
-            System.out.println("[1] Ajouter une route");
-            System.out.println("[2] Terminer l'ajout de routes");
-            int choix = scanner.nextInt();
-            scanner.nextLine(); // Consommer le reste de la ligne après le nombre
+        /*Utilisateur spécifie le fichier/*/- */
+        System.out.println("Entrez le chemin vers le fichier de la communauté d'agglomération :");
+        String cheminFichier = scanner.nextLine();
+        File file = new File(cheminFichier);
 
-
-            switch (choix) {
-                case 1:
-                    System.out.println("Entrez la première ville (lettre):");
-                    char villeA = scanner.nextLine().toUpperCase().charAt(0);
-
-                    System.out.println("Entrez la deuxième ville (lettre):");
-                    char villeB = scanner.nextLine().toUpperCase().charAt(0);
-
-                    ca.ajouterRoute(villeA, villeB);
-                    break;
-                case 2:
-                    fin = true;
-                    break;
-                default:
-                    System.out.println("Choix non valide. Veuillez entrer 1 ou 2.");
-                    break;
-            }
+        try {
+            ca = CommunauteAgglomeration.chargerDepuisFichier(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Erreur : Fichier non trouvé.");
+            return;
         }
 
-        /*  Menu pour gérer les zones de recharge*/
-        fin = false;
-        while (!fin) {
-            System.out.println("[1] Ajouter une zone de recharge");
-            System.out.println("[2] Retirer une zone de recharge");
-            System.out.println("[3] Terminer la configuration");
-            int action = scanner.nextInt();
-            scanner.nextLine(); 
+        Menu.gererRoutes(scanner, ca);
+        Menu.gererZonesDeRecharge(scanner, ca);
 
-            switch (action) {
-                case 1:
-                    System.out.println("Entrez la ville pour ajouter la zone de recharge:");
-                    char villeAjouter = scanner.nextLine().toUpperCase().charAt(0);
-                    ca.ajouterZoneDeRecharge(villeAjouter);
-                    break;
-                case 2:
-                    System.out.println("Entrez la ville pour retirer la zone de recharge:");
-                    char villeRetirer = scanner.nextLine().toUpperCase().charAt(0);
-                    ca.retirerZoneDeRecharge(villeRetirer);
-                    break;
-                case 3:
-                    fin = true;
-                    break;
-                default:
-                    System.out.println("Choix non valide. Veuillez entrer 1, 2 ou 3.");
-                    break;
-            }
-        }
-
-        /*  Afficher la matrice d'adjacence montrant les routes existantes ainsi que les villes contenant des zones de recharge*/
         ca.afficherEtat();
-
-        /* Ferme les flux d'entrées-sorties */
         scanner.close();
     }
 }
