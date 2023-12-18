e33a0dcbee759a35898176d64f7d759b4164c95c
janishmt <152104861+janishmt@users.noreply.github.com>
2023-12-18 23:28:18 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,88 @@
 package projetS5;
 
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.Scanner;
-
-
 
 public class Main {
     public static void main(String[] args) {
+
+        
         Scanner scanner = new Scanner(System.in);
+        CommunauteAgglomeration ca = null;
 
-        CommunauteAgglomeration ca;
-
-        /*Utilisateur spécifie le fichier/*/- */
-        System.out.println("Entrez le chemin vers le fichier de la communauté d'agglomération :");
-        String cheminFichier = scanner.nextLine();
-        File file = new File(cheminFichier);
-
-        try {
-            ca = CommunauteAgglomeration.chargerDepuisFichier(file);
-        } catch (FileNotFoundException e) {
-            System.out.println("Erreur : Fichier non trouvé.");
+        if (args.length != 1) {
+            System.out.println("Veuillez fournir le chemin vers le fichier texte représentant la communauté d'agglomération.");
+            scanner.close();
             return;
         }
 
-        Menu.gererRoutes(scanner, ca);
-        Menu.gererZonesDeRecharge(scanner, ca);
+        try {
+            // Charger la communauté d'agglomération depuis le fichier au démarrage
+            ca = CommunauteAgglomeration.chargerDepuisFichier(new File(args[0]));
+        } catch (FileNotFoundException e) {
+            System.out.println("Le fichier spécifié n'a pas été trouvé.");
+            e.printStackTrace();
+            scanner.close();
+            return;
+        }
 
-        ca.afficherEtat();
+        int choix;
+        do {
+            // Afficher le menu
+            System.out.println("Menu:");
+            System.out.println("1) Resoudre manuellement");
+            System.out.println("2) Resoudre automatiquement");
+            System.out.println("3) Sauvegarder");
+            System.out.println("4) Fin");
+
+            // Lire le choix de l'utilisateur
+            System.out.print("Votre choix : ");
+            choix = scanner.nextInt();
+            scanner.nextLine(); // Consommer la nouvelle ligne
+
+            // Exécuter l'action en fonction du choix de l'utilisateur
+            switch (choix) {
+                case 1:
+                if (ca == null) {
+                    ca = CommunauteAgglomeration.initialiserCommunaute(scanner);
+                }
+            
+                if (!ca.estAccessible()) {
+                    System.out.println("La communauté actuelle ne respecte pas la contrainte d'accessibilité.");
+                    System.out.println("Initialisation avec la solution naïve (une zone de recharge dans chaque ville).");
+                    ca.fullBornedeRecharge();
+                }
+            
+                Menu.gererRoutes(scanner, ca);
+                Menu.gererZonesDeRecharge(scanner, ca);
+                ca.afficherEtat();
+                break;                case 2:
+                    // Résoudre automatiquement
+                    // Appelez votre algorithme ici
+                    break;
+                case 3:
+                    // Sauvegarder
+                    System.out.print("Entrez le chemin vers le fichier de sauvegarde : ");
+                    String cheminSauvegarde = scanner.nextLine();
+                    try {
+                        ca.sauvegarderDansFichier(new File(cheminSauvegarde));
+                        System.out.println("Communauté d'agglomération sauvegardée avec succès.");
+                    } catch (IOException e) {
+                        System.out.println("Erreur lors de la sauvegarde de la communauté d'agglomération dans le fichier.");
+                        e.printStackTrace(); // À adapter selon les besoins de gestion des erreurs
+                    }
+                    break;
+                case 4:
+                    // Fin
+                    System.out.println("Programme terminé.");
+                    break;
+                default:
+                    System.out.println("Choix non valide. Veuillez entrer un nombre entre 1 et 4.");
+                    break;
+            }
+        } while (choix != 4);
+
         scanner.close();
     }
 }
+
